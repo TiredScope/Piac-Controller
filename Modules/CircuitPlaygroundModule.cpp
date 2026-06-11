7637e4c4c74642ef91dc8e3349f47969d27d294c
MrLetsplay <mr.letsplay2003@gmail.com>
2026-06-11 12:44:28 +0200

    Update CircuitPlaygroundModule (WIP)

--- Modules/CircuitPlaygroundModule.cpp
+++ Modules/CircuitPlaygroundModule.cpp
@@ -1,3 +1,4 @@
+#include "Adafruit_Circuit_Playground.h"
 #include "Arduino.h"
 #include "CircuitPlaygroundModule.hpp"
 #include "Adafruit_CircuitPlayground.h"
@@ -16,6 +17,89 @@ void CircuitPlaygroundModule::update() {
   //MiniCom::send(MessageBuilder::build());
 }
 
-void CircuitPlaygroundModule::onMessage(Message) {
+void CircuitPlaygroundModule::onMessage(Message m) {
   // Nothing to handle
+  size_t idx = 0;
+  switch (m.getType()) {
+    case MessageType::M_CIRCUITPLAYGROUND_SET_REPORTING_MODE:
+      {
+        uint8_t reportingMode = m.getU8(idx);
+        this->reportingMode = reportingMode;
+        break;
+      }
+    case MessageType::M_CIRCUITPLAYGROUND_SET_REPORTING_DELAY:
+      {
+        uint8_t target = m.getU8(idx);
+        uint32_t delay = m.getU32(idx);
+
+        if (target & CIRCUITPLAYGROUND_BUTTONS) {
+          // Buttons are not sent continuously
+        }
+
+        if (target & CIRCUITPLAYGROUND_SLIDE_SWITCH) {
+          // Switch is not sent continuously
+        }
+
+        if (target & CIRCUITPLAYGROUND_CAPTOUCH) {
+          capTouchDelay = delay;
+        }
+
+        if (target & CIRCUITPLAYGROUND_ACCELEROMETER) {
+          accelerometerDelay = delay;
+        }
+
+        if (target & CIRCUITPLAYGROUND_TEMPERATURE) {
+          temperatureDelay = delay;
+        }
+
+        if (target & CIRCUITPLAYGROUND_LIGHT) {
+          lightDelay = delay;
+        }
+
+        if (target & CIRCUITPLAYGROUND_SOUND) {
+          soundDelay = delay;
+        }
+
+        break;
+      }
+    case MessageType::M_CIRCUITPLAYGROUND_SET_PIXEL:
+      {
+        uint8_t pixel = m.getU8(idx);
+        uint8_t r = m.getU8(idx);
+        uint8_t g = m.getU8(idx);
+        uint8_t b = m.getU8(idx);
+        CircuitPlayground.setPixelColor(pixel, r, g, b);
+        break;
+      }
+    case MessageType::M_CIRCUITPLAYGROUND_SET_BRIGHTNESS:
+      {
+        uint16_t brightness = m.getU8(idx);
+        CircuitPlayground.setBrightness(brightness);
+        break;
+      }
+      /*case M_CIRCUITPLAYGROUND_BUTTONS:
+      {
+        break;
+      }
+    case M_CIRCUITPLAYGROUND_CAPTOUCH:
+      {
+        break;
+      }
+    case M_CIRCUITPLAYGROUND_ACCELEROMETER:
+      {
+        break;
+      }
+    case M_CIRCUITPLAYGROUND_TEMPERATURE:
+      {
+        break;
+      }
+    case M_CIRCUITPLAYGROUND_LIGHT:
+      {
+        break;
+      }
+    case M_CIRCUITPLAYGROUND_SOUND:
+      {
+        break;
+      }*/
+  }
 }
--- Modules/CircuitPlaygroundModule.hpp
+++ Modules/CircuitPlaygroundModule.hpp
@@ -5,9 +5,24 @@
 #include "../Module.hpp"
 #include "../Common.hpp"
 
+#define CIRCUITPLAYGROUND_DEFAULT_DELAY 1000
+
+#define CIRCUITPLAYGROUND_BUTTONS (1 << 0)
+#define CIRCUITPLAYGROUND_SLIDE_SWITCH (1 << 1)
+#define CIRCUITPLAYGROUND_CAPTOUCH (1 << 2)
+#define CIRCUITPLAYGROUND_ACCELEROMETER (1 << 3)
+#define CIRCUITPLAYGROUND_TEMPERATURE (1 << 4)
+#define CIRCUITPLAYGROUND_LIGHT (1 << 5)
+#define CIRCUITPLAYGROUND_SOUND (1 << 6)
+
 class CircuitPlaygroundModule : public Module {
 private:
-  pin_t recvPin;
+  uint8_t reportingMode = 0;  // 0 = all disabled
+  uint32_t capTouchDelay = CIRCUITPLAYGROUND_DEFAULT_DELAY;
+  uint32_t accelerometerDelay = CIRCUITPLAYGROUND_DEFAULT_DELAY;
+  uint32_t temperatureDelay = CIRCUITPLAYGROUND_DEFAULT_DELAY;
+  uint32_t lightDelay = CIRCUITPLAYGROUND_DEFAULT_DELAY;
+  uint32_t soundDelay = CIRCUITPLAYGROUND_DEFAULT_DELAY;
 public:
   CircuitPlaygroundModule(uint8_t discriminator = DEFAULT_DISCRIMINATOR)
     : Module(discriminator){};
